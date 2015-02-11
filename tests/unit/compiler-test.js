f3c5314cd9bc7863a7a30c0a148b080459b54386
Cory Forsyth <cory.forsyth@gmail.com>
2015-02-11 18:24:43 -0500

    Refactor parser tests with helpers astTest and ast helpers

--- tests/unit/compiler-test.js
+++ tests/unit/compiler-test.js
@@ -5,153 +5,6 @@ import { generateBuilder } from '../../emblem/ast-builder';
 
 QUnit.module("Unit - parse");
 
-var expectations = [
-  {
-    name: 'simple text',
-    emblem: '| abc def ghi',
-    ast: makeAST({
-      type: 'text', content: 'abc def ghi'
-    })
-  }, {
-    name: 'multiline text',
-    emblem: ['| abc def ghi',
-             '  another line'].join('\n'),
-    ast: makeAST({
-      type: 'text', content: 'abc def ghi\nanother line'
-    })
-  }, {
-    name: 'simple element',
-    emblem: 'h1 my great element',
-    ast: makeAST({
-      type: 'element',
-      tagName: 'h1',
-      attrStaches: [],
-      childNodes: [{
-        type: 'text',
-        content: 'my great element'
-      }]
-    })
-  }, {
-    name: 'simple element with single class name',
-    emblem: 'h1.my-class',
-    ast: makeAST({
-      type: 'element',
-      tagName: 'h1',
-      attrStaches: [{
-        type: 'attribute',
-        name: 'class',
-        content: 'my-class'
-      }],
-      childNodes: []
-    })
-  }, {
-    name: 'simple element with id',
-    emblem: 'h1#my-id',
-    ast: makeAST({
-      type: 'element',
-      tagName: 'h1',
-      attrStaches: [{
-        type: 'attribute',
-        name: 'id',
-        content: 'my-id'
-      }],
-      childNodes: []
-    })
-  }, {
-    name: 'simple element with id and class',
-    emblem: 'h1#my-id.my-class',
-    ast: makeAST({
-      type: 'element',
-      tagName: 'h1',
-      attrStaches: [{
-        type: 'attribute',
-        name: 'id',
-        content: 'my-id'
-      }, {
-        type: 'attribute',
-        name: 'class',
-        content: 'my-class'
-      }],
-      childNodes: []
-    })
-  }, {
-    name: 'element with shorthand attribute syntax',
-    emblem: '#my-id.my-class',
-    ast: makeAST({
-      type: 'element',
-      tagName: 'div',
-      attrStaches: [{
-        type: 'attribute',
-        name: 'id',
-        content: 'my-id'
-      }, {
-        type: 'attribute',
-        name: 'class',
-        content: 'my-class'
-      }],
-      childNodes: []
-    })
-  }, {
-    name: 'special element name',
-    emblem: '%blink',
-    ast: makeAST({
-      type: 'element',
-      tagName: 'blink',
-      attrStaches: [],
-      childNodes: []
-    })
-  }, {
-    name: 'simple nested elements',
-    emblem: ['ul',
-             '  li'].join('\n'),
-    ast: makeAST({
-      type: 'element',
-      tagName: 'ul',
-      attrStaches: [],
-      childNodes: [{
-        type: 'element',
-        tagName: 'li',
-        attrStaches: [],
-        childNodes: []
-      }]
-    })
-  }, {
-    name: 'nested elements with content',
-    emblem: ['ul',
-             '  li hello',
-             '  li goodbye'].join('\n'),
-    ast: makeAST({
-      type: 'element',
-      tagName: 'ul',
-      attrStaches: [],
-      childNodes: [{
-        type: 'element',
-        tagName: 'li',
-        attrStaches: [],
-        childNodes: [{
-          type: 'text',
-          content: 'hello'
-        }]
-      }, {
-        type: 'element',
-        tagName: 'li',
-        attrStaches: [],
-        childNodes: [{
-          type: 'text',
-          content: 'goodbye'
-        }]
-      }]
-    })
-  }
-];
-
-function makeAST(childNode){
-  return {
-    type: 'program',
-    childNodes: [childNode]
-  };
-}
-
 function truncate(text, len){
   if (!len) { len = 40; }
   text = text.replace(/\n/g, "\\n");
@@ -159,54 +12,154 @@ function truncate(text, len){
   return text.slice(0, len) + ellipses;
 }
 
-var expectation, emblem, testName, expectedAST, message;
-
-for (var i=0, l=expectations.length; i<l; i++){
-  expectation = expectations[i];
-
-  emblem = expectation.emblem;
-  testName = expectation.name;
-  expectedAST = expectation.ast;
-  message = expectation.message || 'should work';
-
-  QUnit.test( testName + ' (' + truncate(emblem) + ')', function(assert) {
+function astTest(name, emblem, callback){
+  QUnit.test(name + ' "' + truncate(emblem) + '"', function(assert){
     var builder = generateBuilder();
+    debugger;
     parse( processSync(emblem), {builder:builder} );
     var ast = builder.toAST();
 
-    assert.deepEqual(ast, expectedAST, message);
+    callback(assert, ast);
   });
 }
 
-/*
-QUnit.test('parses nested elements interspersed with content', function(assert){
-  var emblem = ['p',
-                '  blah blah',
-                '  b bold blah blah'].join('\n');
-
-  var builder = generateBuilder();
-  parse( processSync(emblem), {builder:builder});
-  var ast = builder.toAST();
-
-  assert.deepEqual(ast, {
+function program(childNodes){
+  return {
     type: 'program',
-    childNodes: [{
-      type: 'element',
-      tagName: 'p',
-      attrStaches: [],
-      childNodes: [{
-        type: 'text',
-        content: 'blah blah'
-      }, {
-        type: 'element',
-        tagName: 'b',
-        attrStaches: [],
-        childNodes: [{
-          type: 'text',
-          content: 'bold blah blah'
-        }]
-      }]
-    }]
-  });
+    childNodes: childNodes
+  };
+}
+
+function element(tagName, childNodes, attrStaches){
+  return {
+    type: 'element',
+    tagName: tagName,
+    attrStaches: attrStaches || [],
+    childNodes: childNodes || []
+  };
+}
+
+function text(content){
+  return {
+    type: 'text',
+    content: content
+  };
+}
+
+function attribute(attrName, attrContent){
+  return {
+    type: 'attribute',
+    name: attrName,
+    content: attrContent
+  };
+}
+
+astTest('simple element', 'h1 hello', function(assert, ast){
+  assert.deepEqual(ast, program([ element('h1', [ text('hello') ] ) ]) );
+});
+
+astTest('simple text', '| abc def ghi', function(assert, ast){
+  assert.deepEqual(ast, program([ text('abc def ghi') ]) );
+});
+
+astTest('multiline text',
+        ['| abc def ghi',
+         '  another line'].join('\n'), function(assert, ast){
+  assert.deepEqual(ast, program([text('abc def ghi\nanother line')]));
+});
+
+astTest('simple element', 'h1 my great element', function(assert, ast){
+  assert.deepEqual(ast, program([element('h1', [text('my great element')])]));
+});
+
+astTest('simple element with single class name', 'h1.my-class', function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([element('h1', [], [attribute('class', 'my-class')])])
+  );
+});
+
+astTest('simple element with id', 'h1#my-id', function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([ element('h1', [], [attribute('id', 'my-id')]) ])
+  );
+});
+
+astTest('simple element with id and class', 'h1#my-id.my-class', function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([ element('h1', [], [attribute('id', 'my-id'),
+                                 attribute('class', 'my-class')]) ])
+  );
+});
+
+astTest('element with shorthand attributes', '#my-id.my-class', function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([ element('div', [], [attribute('id', 'my-id'),
+                                 attribute('class', 'my-class')]) ])
+  );
+});
+
+astTest('special element', '%blink', function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([ element('blink', [], []) ])
+  );
+});
+
+astTest('simple nested elements',
+        ['ul',
+         '  li'].join('\n'), function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([ element('ul', [element('li')]) ])
+  );
+});
+
+astTest('simple nested elements with content',
+        ['ul',
+         '  li hello',
+         '  li goodbye'].join('\n'), function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([
+      element('ul',
+              [
+                element('li', [text('hello')]),
+                element('li', [text('goodbye')])
+              ])
+    ])
+  );
+});
+
+astTest('html attributes', 'button.close data-dismiss="modal" x', function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([
+      element('button',
+              [text('x')],
+              [attribute('data-dismiss', 'modal'),
+               attribute('class', 'close')])
+    ])
+  );
+});
+
+/* FIXME --this is most likely valid emblem and this test should work
+astTest('nested elements interspersed with content',
+        ['p',
+         '  blah blah',
+         '  b bold text'].join('\n'), function(assert, ast){
+  assert.deepEqual(
+    ast,
+    program([
+      element('p',
+              [
+                text('blah blah'),
+                element('b', [text('bold text')])
+              ])
+    ])
+  );
 });
 */

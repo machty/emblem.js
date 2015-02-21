3507359722665e208ac60d8b55376df06511dea7
Cory Forsyth <cory.forsyth@gmail.com>
2015-02-21 17:17:06 -0500

    Add tests, FIXME for Vanilla handlebars

---
+++ tests/integration/vanilla-handlebars-support-test.js
@@ -0,0 +1,7 @@
+// FIXME handle ">" partial invocation
+//
+// from http://emblemjs.com/syntax/
+//
+// "> partialName" -> "{{>partialName}}"
+// "p check out #{> partialName}" -> "<p>check out {{>partialName}}</p>"
+// "> partialName foo" -> "{{>partialName foo}}"

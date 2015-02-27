bef7cf6f46b4445c43fef2e15ea3cb53e6fa00a6
Matthew Beale <matt.beale@madhatted.com>
2015-02-27 16:57:51 -0500

    Of dubious worth, this walker might be interesting

---
+++ bin/example-file-walker.js
@@ -0,0 +1,37 @@
+var walk = require('walk');
+var fs = require('fs');
+var path = require('path');
+var emblem = require('emblem');
+var compile = emblem.default.compile;
+
+var walker = walk.walk("app/templates");
+var compiledCount = 0, totalCount = 0, erroredCount = 0;
+
+walker.on("file", fileHandler);
+
+function fileHandler(root, fileStat, next){
+  if (path.extname(fileStat.name) !== ".emblem") {
+    console.log('skipping ' + fileStat.name);
+    return next();
+  }
+  var filePath = path.resolve(root, fileStat.name);
+  var contents = fs.readFileSync(filePath, {encoding:'utf8'});
+
+  try {
+    var compiled = compile(contents);
+    compiledCount++;
+
+    console.log('compiled!: ',filePath);
+  } catch(e) {
+    erroredCount++;
+    console.log('ERRORED',filePath,contents);
+  }
+
+  totalCount++;
+
+  next();
+}
+
+walker.on("end", function(){
+  console.log({totalCount:totalCount, erroredCount:erroredCount, compiledCount:compiledCount});
+});

9a52a5cf1eb012727530d322c9345484749b5970
John Ratcliff <john.w.ratcliff@biola.edu>
2018-12-22 05:29:54 +0000

    Fix #318 Add spread attributes to glimmer components

--- tests/integration/glimmer-component-test.js
+++ tests/integration/glimmer-component-test.js
@@ -7,36 +7,44 @@ QUnit.module("glimmer-components");
 
 QUnit.test("basic syntax", function(assert) {
   var emblem = w(
-    "% my-component @value=foo data-hint='not-my-component%%::'"
+    "%MyComponent @value=foo data-hint='not-my-component%%::'"
   );
   assert.compilesTo(emblem,
-    '<my-component @value={{foo}} data-hint=\"not-my-component%%::\"></my-component>');
+    '<MyComponent @value={{foo}} data-hint=\"not-my-component%%::\"></MyComponent>');
 });
 
 QUnit.test("basic syntax with legacy quoting", function(assert) {
   var emblem = w(
-    "% my-component value=foo data-hint='not-my-component%%::'"
+    "%MyComponent value=foo data-hint='not-my-component%%::'"
   );
   assert.compilesTo(emblem,
-    '<my-component value=\"{{foo}}\" data-hint=\"not-my-component%%::\"></my-component>', null, {
+    '<MyComponent value=\"{{foo}}\" data-hint=\"not-my-component%%::\"></MyComponent>', null, {
       legacyAttributeQuoting: true
     });
 });
 
 QUnit.test("boolean attribute passed in as component input", function(assert) {
   var emblem = w(
-    "% my-component @multiselect=false"
+    "%MyComponent @multiselect=false"
   );
   assert.compilesTo(emblem,
-    '<my-component @multiselect={{false}}></my-component>');
+    '<MyComponent @multiselect={{false}}></MyComponent>');
+});
+
+QUnit.test("...attributes", function(assert) {
+  var emblem = w(
+    "%MyComponent ...attributes type=@post.type"
+  );
+  assert.compilesTo(emblem,
+    '<MyComponent ...attributes type={{@post.type}}></MyComponent>');
 });
 
 QUnit.test("names with :", function(assert) {
   var emblem = w(
-    "% inputs:my-component @value=foo"
+    "% inputs:MyComponent @value=foo"
   );
   assert.compilesTo(emblem,
-    '<inputs:my-component @value={{foo}}></inputs:my-component>');
+    '<inputs:MyComponent @value={{foo}}></inputs:MyComponent>');
 });
 
 // @TODO
@@ -44,20 +52,20 @@ QUnit.test("names with :", function(assert) {
 
 QUnit.test("Blocks", function(assert) {
    var emblem = w(
-    "% my-component @value=foo",
+    "%MyComponent @value=foo",
     "  |Hi!"
   );
   assert.compilesTo(emblem,
-    '<my-component @value={{foo}}>Hi!</my-component>');
+    '<MyComponent @value={{foo}}>Hi!</MyComponent>');
 });
 
 QUnit.test("Block params", function(assert) {
    var emblem = w(
-    "% my-component @value=foo as |comp1 comp2|",
+    "%MyComponent @value=foo as |comp1 comp2|",
     "  = comp.name"
   );
   assert.compilesTo(emblem,
-    '<my-component @value={{foo}} as |comp1 comp2|>{{comp.name}}</my-component>');
+    '<MyComponent @value={{foo}} as |comp1 comp2|>{{comp.name}}</MyComponent>');
 });
 
 // @TODO: What should the result of this be?
@@ -65,21 +73,21 @@ QUnit.test("Block params", function(assert) {
 
 QUnit.test('brackets with string', function(assert) {
   var emblem = w('',
-                 '%my-component [',
+                 '%MyComponent [',
                  '  @foo=bar',
                  '  @baz=\'food\' ]');
   assert.compilesTo(
-    emblem, '<my-component @foo={{bar}} @baz=\"food\"></my-component>');
+    emblem, '<MyComponent @foo={{bar}} @baz=\"food\"></MyComponent>');
 });
 
 QUnit.test('brackets with dedent end', function(assert) {
   var emblem = w('',
-                 '%my-component [',
+                 '%MyComponent [',
                  '  @foo=bar',
                  '  @baz=\'food\'',
                  ']');
   assert.compilesTo(
-    emblem, '<my-component @foo={{bar}} @baz=\"food\"></my-component>');
+    emblem, '<MyComponent @foo={{bar}} @baz=\"food\"></MyComponent>');
 });
 
 // Invalid
@@ -87,32 +95,32 @@ QUnit.test('brackets with dedent end', function(assert) {
 
 QUnit.test('bracketed nested block', function(assert) {
   var emblem = w('',
-                 '%my-component [',
+                 '%MyComponent [',
                  '  ',
                  '  @something="false" ]',
                  '  p Bracketed helper attrs!');
   assert.compilesTo(
-    emblem, '<my-component @something=\"false\"><p>Bracketed helper attrs!</p></my-component>');
+    emblem, '<MyComponent @something=\"false\"><p>Bracketed helper attrs!</p></MyComponent>');
 });
 
 QUnit.test('bracketed nested with actions', function(assert) {
   var emblem = w('',
-                 '%my-component [',
+                 '%MyComponent [',
                  '  onclick={ action \'doSometing\' foo bar }',
                  '  change=\'otherAction\'',
                  '  @something="false" ]',
                  '  p Bracketed helper attrs!');
   assert.compilesTo(
-    emblem, '<my-component onclick={{action \'doSometing\' foo bar}} {{action \"otherAction\" on=\"change\"}} @something=\"false\"><p>Bracketed helper attrs!</p></my-component>');
+    emblem, '<MyComponent onclick={{action \'doSometing\' foo bar}} {{action \"otherAction\" on=\"change\"}} @something=\"false\"><p>Bracketed helper attrs!</p></MyComponent>');
 });
 
-// @TODO: should these support mustache-like syntax?  (i.e. %my-component value=(foo) )
+// @TODO: should these support mustache-like syntax?  (i.e. %MyComponent value=(foo) )
 QUnit.test("Sub-expressions", function(assert) {
    var emblem = w(
-    "% my-component @value={ (or (eq foo 'bar') (eq foo 'baz')) }"
+    "%MyComponent @value={ (or (eq foo 'bar') (eq foo 'baz')) }"
   );
   assert.compilesTo(emblem,
-    '<my-component @value={{(or (eq foo \'bar\') (eq foo \'baz\'))}}></my-component>');
+    '<MyComponent @value={{(or (eq foo \'bar\') (eq foo \'baz\'))}}></MyComponent>');
 });
 
 QUnit.test('recursive nesting part 2', function(assert) {

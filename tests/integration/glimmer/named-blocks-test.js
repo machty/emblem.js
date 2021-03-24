6d674da5659d6ce5ae331fbfb95e8e9e3b09ec4c
Evgeni B <evgeni.hypha@gofree.club>
2021-03-25 00:49:13 +0300

    Added test case for https://github.com/machty/emblem.js/issues/333 but actually that works

--- tests/integration/glimmer/named-blocks-test.js
+++ tests/integration/glimmer/named-blocks-test.js
@@ -15,4 +15,17 @@ module('glimmer: named blocks', function (hooks) {
 
     assert.compilesTo(emblem, '<x-modal><@header as |@title|>Header {{title}}</@header><@body>Body {{title}}</@body><@footer>Footer</@footer></x-modal>');
   });
+
+  test('named yielded block support', function (assert) {
+    const emblem = w(
+      '%Article',
+      '  %:title',
+      '    h1 = this.title',
+      '  %:body',
+      '    .byline = byline this.author',
+      '    .body = this.body',
+    );
+
+    assert.compilesTo(emblem, '<Article><:title><h1>{{this.title}}</h1></:title><:body><div class="byline">{{byline this.author}}</div><div class="body">{{this.body}}</div></:body></Article>');
+  });
 });
